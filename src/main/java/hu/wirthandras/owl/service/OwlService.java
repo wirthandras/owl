0c68d30f518738d81274337c2eb263b9a27b8e2b
Andras Wirth <agrippa.wia@gmail.com>
2019-03-04 17:49:30 +0100

    OwlServiceTest added

--- src/main/java/hu/wirthandras/owl/service/OwlService.java
+++ src/main/java/hu/wirthandras/owl/service/OwlService.java
@@ -1,13 +1,14 @@
 package hu.wirthandras.owl.service;
 
+import static java.util.Collections.reverseOrder;
+import static java.util.Collections.sort;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import hu.wirthandras.owl.domain.Translation;
 import hu.wirthandras.owl.domain.comparator.FrequentComparator;
@@ -18,37 +19,36 @@ public class OwlService {
 	private final int ELEMENTS_IN_LIST = 10;
 	private final int START_INDEX = 0;
 
-	@Value("${owl.target.server.url}")
-	private String path;
-
-	public Translation[] getTexts() {
-		RestTemplate restTemplate = new RestTemplate();
-		Translation[] result = restTemplate.getForObject(path, Translation[].class);
-		return result;
-	}
+	@Autowired
+	private RemoteService remoteService;
 
 	public List<Translation> getBestTen() {
-		List<Translation> t = new ArrayList<Translation>(Arrays.asList(getTexts()));
-		Collections.sort(t, Collections.reverseOrder());
+		List<Translation> t = getTranslations();
+		sort(t, reverseOrder());
 		return t.subList(START_INDEX, ELEMENTS_IN_LIST);
 	}
 
 	public List<Translation> getWorstTen() {
-		List<Translation> t = new ArrayList<Translation>(Arrays.asList(getTexts()));
-		Collections.sort(t);
+		List<Translation> t = getTranslations();
+		sort(t);
 		return t.subList(START_INDEX, ELEMENTS_IN_LIST);
 	}
 
 	public List<Translation> getFrequentTen() {
-		List<Translation> t = new ArrayList<Translation>(Arrays.asList(getTexts()));
-		Collections.sort(t, Collections.reverseOrder(new FrequentComparator()));
+		List<Translation> t = getTranslations();
+		sort(t, reverseOrder(new FrequentComparator()));
 		return t.subList(START_INDEX, ELEMENTS_IN_LIST);
 	}
 
 	public List<Translation> getRareTen() {
-		List<Translation> t = new ArrayList<Translation>(Arrays.asList(getTexts()));
-		Collections.sort(t, new FrequentComparator());
+		List<Translation> t = getTranslations();
+		sort(t, new FrequentComparator());
 		return t.subList(START_INDEX, ELEMENTS_IN_LIST);
+	}
+
+	private List<Translation> getTranslations() {
+		List<Translation> t = new ArrayList<Translation>(Arrays.asList(remoteService.getTexts()));
+		return t;
 	}
 
 }
